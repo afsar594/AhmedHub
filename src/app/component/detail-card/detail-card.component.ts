48f7a2e8e325023171b68fac0968984348a6185f
AfsarRiaz <malikafsar594@gmail.com>
2025-12-04 21:47:59 +0500

    update

--- src/app/component/detail-card/detail-card.component.ts
+++ src/app/component/detail-card/detail-card.component.ts
@@ -5,40 +5,49 @@ import { Component } from '@angular/core';
   selector: 'app-detail-card',
   imports: [CommonModule],
   templateUrl: './detail-card.component.html',
-  styleUrl: './detail-card.component.css'
+  styleUrl: './detail-card.component.css',
 })
 export class DetailCardComponent {
-  quantity=1;
-product = [
-  {
-  title: 'Men’s Classic Warm Hoodie',
-  image: 'https://i.pinimg.com/1200x/85/06/26/850626136d53b0ec52bc37e9a35f4c1f.jpg',
-  price: 15.99,
-  oldPrice: 22.99,
-  discount: '-30%'
-},
-]
-totalPriceFix(p:any,q:any)
-{
-let total=p*q;
-return total.toFixed(2);
-}
-increase(){
-  this.quantity++;
-}
-decrease(){
-  if(this.quantity>1){
-    this.quantity--
+  quantity = 1;
+  product = [
+    {
+      title: 'Men’s Classic Warm Hoodie',
+      image:
+        'https://i.pinimg.com/1200x/85/06/26/850626136d53b0ec52bc37e9a35f4c1f.jpg',
+      price: 15.99,
+      oldPrice: 22.99,
+      discount: '-30%',
+    },
+  ];
+  totalPriceFix(p: any, q: any) {
+    let total = p * q;
+    return total.toFixed(2);
   }
-}
-//  selectColor(color:string){
-//   this.selectColor=color;
-//  }
-   colors=[
-  '#000000', '#ff0000', '#0066ff', '#ff9900', '#7d00ff', '#F54927' ,'#F5276C' , '#F5B027' , '#CD5C5C' , '#808080' , '#008000' 
- ]
- selectcolor='';
- sizes=['S' , 'M' , 'L' , 'XL'];
- selectedsize='';
-
+  increase() {
+    this.quantity++;
+  }
+  decrease() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+  //  selectColor(color:string){
+  //   this.selectColor=color;
+  //  }
+  colors = [
+    '#000000',
+    '#ff0000',
+    '#0066ff',
+    '#ff9900',
+    '#7d00ff',
+    '#F54927',
+    '#F5276C',
+    '#F5B027',
+    '#CD5C5C',
+    '#808080',
+    '#008000',
+  ];
+  selectcolor = '';
+  sizes = ['S', 'M', 'L', 'XL'];
+  selectedsize = '';
 }

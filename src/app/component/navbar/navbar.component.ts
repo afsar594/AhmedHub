f76c29936c4df985c6402c11efb3fe831cb15d14
AfsarRiaz <malikafsar594@gmail.com>
2025-12-14 20:08:54 +0500

    update

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -3,19 +3,25 @@ import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  menuOpen = false;
+
   constructor(private router: Router) {}
 
-  navigateTocart(): void {
-    this.router.navigate(['cart-page']);
-  }
-  menuOpen: boolean = false;
-
-  toggleMenu() {
+  toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
+
+  navigateToCart(): void {
+    this.router.navigate(['cart-page']);
   }
 }
